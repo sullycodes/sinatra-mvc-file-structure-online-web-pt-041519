ff349188514ed64910d03cfcafc6088e38ff6ef9
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-18 19:29:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,13 @@ class ApplicationController < Sinatra::Base
   
 end
 
-class Dog(name, breed, age) 
+class Dog
+  
+attr_accessor :name, :breed, :age 
+  
+  def initialize(name, breed, age)
+    @name = name 
+    @breed = breed 
+    @age = age 
+  end
 end
